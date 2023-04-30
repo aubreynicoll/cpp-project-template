af9a414ac18f6de786d96c7e59c570065af1f68c
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2023-04-30 18:49:32 +1000

    update scripts

--- init_build.sh
+++ init_build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-conan install . --output-folder=build --build=missing --profile=default # may want to change profile to release in the future
-cmake -B build . -DCMAKE_TOOLCHAIN_FILE=./build/conan_toolchain.cmake
+# use 1st arg, or Debug as default
+BUILD_TYPE="${1:-Debug}"
+
+./make_clean.sh
+conan install . --output-folder=build --build=missing --settings=build_type="$BUILD_TYPE"
+cmake -B build . -DCMAKE_TOOLCHAIN_FILE=./build/conan_toolchain.cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 cmake --build build
