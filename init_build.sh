6f323e185313159db24d1308fead4297a590e553
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2023-05-01 16:00:57 +1000

    add sanitizers

--- init_build.sh
+++ init_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # use 1st arg, or Debug as default
-BUILD_TYPE="${1:-Debug}"
+BUILD_TYPE="${1:-debug}"
 
 ./make_clean.sh
-conan install . --output-folder=build --build=missing --settings=build_type="$BUILD_TYPE"
+conan install . --output-folder=build --build=missing
 cmake -B build . -DCMAKE_TOOLCHAIN_FILE=./build/conan_toolchain.cmake -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 cmake --build build
